db71278e413d8ed417454b3dddc678c613cc2829
AndrewVetovitz <AndrewVetovitz@gmail.com>
2025-05-11 04:19:14 -0400

    fixing footer aria text

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -6,11 +6,31 @@ import { faFacebook, faGithub, faInstagram, faLinkedin } from "@fortawesome/free
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons/faEnvelope";
 
 const links = [
-  { text: "Facebook", href: "https://www.facebook.com/profile.php?id=100069238363197/", icon: faFacebook },
-  { text: "linkedin", href: "https://www.linkedin.com/in/andrewvetovitz/", icon: faLinkedin },
-  { text: "Instagram", href: "https://www.instagram.com/avgunner/", icon: faInstagram },
-  { text: "Github", href: "https://github.com/AndrewVetovitz/", icon: faGithub },
-  { text: "Mail", href: "mailto:andrewvetovitz@gmail.com", icon: faEnvelope },
+  {
+    text: "Facebook",
+    href: "https://www.facebook.com/profile.php?id=100069238363197/",
+    icon: faFacebook,
+    ariaLabel: "Andrew Vetovitz Facebook",
+  },
+  {
+    text: "linkedin",
+    href: "https://www.linkedin.com/in/andrewvetovitz/",
+    icon: faLinkedin,
+    ariaLabel: "Andrew Vetovitz Linkedln",
+  },
+  {
+    text: "Instagram",
+    href: "https://www.instagram.com/avgunner/",
+    icon: faInstagram,
+    ariaLabel: "Andrew Vetovitz Instagram",
+  },
+  { text: "Github", href: "https://github.com/AndrewVetovitz/", icon: faGithub, ariaLabel: "Andrew Vetovitz Github" },
+  {
+    text: "Mail",
+    href: "mailto:andrewvetovitz@gmail.com",
+    icon: faEnvelope,
+    ariaLabel: "Andrew Vetovitz primary mail address",
+  },
 ];
 
 function Footer() {
@@ -18,7 +38,7 @@ function Footer() {
     <div className={"flex flex-col py-10 max-w-5xl m-auto"}>
       <div className={"flex justify-center gap-x-5 py-[10px]"}>
         {links.map((link) => (
-          <a key={link.text} href={link.href} target="_blank" rel="noopener noreferrer">
+          <a key={link.text} aria-label={link.ariaLabel} href={link.href} target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={link.icon} size="2xl" />
           </a>
         ))}
