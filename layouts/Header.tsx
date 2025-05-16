d15fa82c9a98345771badaeeeb60d8f3e4567abe
AndrewVetovitz <AndrewVetovitz@gmail.com>
2025-05-15 20:10:05 -0400

    fix routing issue causing redirect

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -15,15 +15,15 @@ function Header() {
       </div>
       <div id="header" className="flex justify-center gap-x-2 pb-4">
         <Title className="text-2xl">
-          <Link href="/projects">Projects</Link>
+          <Link href="/projects/">Projects</Link>
         </Title>
         <Title className="text-2xl select-none"> • </Title>
         <Title className="text-2xl">
-          <Link href="/about">About</Link>
+          <Link href="/about/">About</Link>
         </Title>
         <Title className="text-2xl select-none"> • </Title>
         <Title className="text-2xl">
-          <Link href="/resume">Resume</Link>
+          <Link href="/resume/">Resume</Link>
         </Title>
       </div>
     </div>
