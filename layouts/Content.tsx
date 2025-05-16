261a911ca6885b29919ac54e48907e19645f78bf
AndrewVetovitz <AndrewVetovitz@gmail.com>
2025-05-15 21:49:30 -0400

    small lint fix and remove extra build command

--- layouts/Content.tsx
+++ layouts/Content.tsx
@@ -1,5 +1,5 @@
 function Content({ children }: { children: React.ReactNode }) {
-  return <div className={"pb-12 max-w grow px-4"}>{children}</div>;
+  return <div className="pb-12 max-w grow px-4">{children}</div>;
 }
 
 export default Content;
